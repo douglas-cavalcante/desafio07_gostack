27d3aa4a3b39008f074b470840dce29d6b25a753
douglas-cavalcante <henriquedouglas2803@gmail.com>
2020-08-27 01:09:09 -0300

    adiciona solucao do desafio

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
+// Formata os valores na moeda e lingua através da função Intl
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
 
 export default formatValue;
